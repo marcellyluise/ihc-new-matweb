0cfc05a451a11bb2c65d395948784ccfc65a3ddb
Marcelly Luise <marcellygodinho@gmail.com>
2018-06-02 21:26:51 -0300

    - First version of Login UI - Change initialization flow to login - Add UnB asset

---
+++ MatWebUnB/MatWebUnB/ViewModel/Authentication/LoginViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LoginViewController.swift
+//  MatWebUnB
+//
+//  Created by Marcelly Luise on 02/06/18.
+//  Copyright © 2018 Marcelly Luise. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupTableView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table View
+    private func setupTableView() {
+        tableView.tableFooterView = UIView()
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 44.0
+    }
+    
+    // MARK: - Actions
+    @IBAction func login(_ sender: Any) {
+        
+    }
+    
+    @IBAction func switchToPos(_ sender: Any) {
+        
+    }
+}
